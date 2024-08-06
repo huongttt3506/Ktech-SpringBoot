b2fc7b9a559658901904b768d5fa732528a85d4c
edujeeho <edujeeho@gmail.com>
2024-08-06 14:35:30 +0900

    insert, update, delete mapping

--- 03_mybatis/src/main/java/com/example/crud/mapper/StudentMapperX.java
+++ 03_mybatis/src/main/java/com/example/crud/mapper/StudentMapperX.java
@@ -5,6 +5,10 @@ import com.example.crud.model.Student;
 import java.util.List;
 
 public interface StudentMapperX {
+    // 메서드 이름과 동일한 id를 가진 XML SQL이 연결된다.
+    void insertStudent(Student student);
     List<Student> selectStudentAll();
     Student selectStudent(Long id);
+    void updateStudent(Student student);
+    void deleteStudent(Long id);
 }
