79c2bcc63b34fe0edadee5469426c39a97ed0d22
edujeeho <edujeeho@gmail.com>
2024-08-07 13:51:24 +0900

    query method

--- 04_jpa/src/main/java/com/example/jpa/TestController.java
+++ 04_jpa/src/main/java/com/example/jpa/TestController.java
@@ -18,31 +18,33 @@ public class TestController {
     // http://localhost:8080/test
     @GetMapping("test")
     public String test() {
-        // CREATE
-        Student newStudent = new Student();
-        newStudent.setName("Alex");
-        newStudent.setEmail("alex@gmail.com");
-        System.out.println(newStudent.getId());  // null
-        Student saved = repository.save(newStudent);
-        System.out.println(saved.getId());
-        System.out.println(saved);
+//        // CREATE
+//        Student newStudent = new Student();
+//        newStudent.setName("Alex");
+//        newStudent.setEmail("alex@gmail.com");
+//        System.out.println(newStudent.getId());  // null
+//        Student saved = repository.save(newStudent);
+//        System.out.println(saved.getId());
+//        System.out.println(saved);
+//
+//        // READ
+//        Student readOne = repository.findById(1L)
+//                .orElse(null);
+//        System.out.println(readOne);
+//        List<Student> readAll = repository.findAll();
+//        System.out.println(readAll.size());
+//
+//        // UPDATE
+//        Student target = repository.findById(1L)
+//                .orElseThrow();
+//        target.setAge(readAll.size());
+//        repository.save(target);
+//
+//        // DELETE
+//        repository.deleteById(2L);
+//        repository.delete(saved);
 
-        // READ
-        Student readOne = repository.findById(1L)
-                .orElse(null);
-        System.out.println(readOne);
-        List<Student> readAll = repository.findAll();
-        System.out.println(readAll.size());
-
-        // UPDATE
-        Student target = repository.findById(1L)
-                .orElseThrow();
-        target.setAge(readAll.size());
-        repository.save(target);
-
-        // DELETE
-        repository.deleteById(2L);
-        repository.delete(saved);
+        System.out.println(repository.findAllByEmailEndingWith("hotmail.org"));
 
         return "test.html";
     }
--- 04_jpa/src/main/java/com/example/jpa/repo/StudentRepository.java
+++ 04_jpa/src/main/java/com/example/jpa/repo/StudentRepository.java
@@ -3,5 +3,12 @@ package com.example.jpa.repo;
 import com.example.jpa.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository
-        extends JpaRepository<Student, Long> {}
+        extends JpaRepository<Student, Long> {
+    /*
+    SELECT * FROM student WHERE email LIKE '%{?}'
+     */
+    List<Student> findAllByEmailEndingWith(String email);
+}
